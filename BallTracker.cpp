3d00cb6da74246554ff897e4532522c64ad8ecfd
s13909 <s13909@pjwstk.edu.pl>
2019-05-29 14:44:03 +0200

    Added contour sorting, line drawing , and basic detection of letter M

--- BallTracker.cpp
+++ BallTracker.cpp
@@ -1,7 +1,13 @@
 
-#include <sstream>
-#include <string>
+
 #include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <list>
+#include <string>
+#include <sstream>
+
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -10,32 +16,11 @@
 using namespace std;
 using namespace cv;
 
-//default capture width and height
-const int FRAME_WIDTH = 800;
-const int FRAME_HEIGHT = 600;
-//max number of objects to be detected in frame
-const int MAX_NUM_OBJECTS = 50;
-//minimum and maximum object area
-const int MIN_OBJECT_AREA = 20 * 20;
-const int MAX_OBJECT_AREA = FRAME_HEIGHT * FRAME_WIDTH / 1.5;
-
-
-
-
 int main(int argc, char* argv[])
 {
 
-
-	
-
-	VideoCapture capture; 	//video capture object to acquire webcam feed
-
-	const int MIN_OBJECT_AREA = 20 * 20;
-	const int MAX_OBJECT_AREA = 800 * 600 / 1.5;
-
-
-
-
+	//video capture object to acquire webcam feed
+	VideoCapture capture; 
 
 	//open capture
 	capture.open(0);
@@ -43,6 +28,9 @@ int main(int argc, char* argv[])
 	//defining width and height of capture
 	capture.set(CV_CAP_PROP_FRAME_WIDTH, 600);
 	capture.set(CV_CAP_PROP_FRAME_HEIGHT, 800);
+
+	//lists
+	list < Point2f > path; //List for saving path
 
 	//loop for displaying webcam feeds
 	while (waitKey(30) != 'q') {
@@ -61,7 +49,7 @@ int main(int argc, char* argv[])
 		inRange(HSV, Scalar(47, 114, 0), Scalar(72, 256, 256), filtered);
 
 		//adding dilate to make object more visible
-		Mat dilateElement = getStructuringElement(MORPH_RECT, Size(10, 10));
+		Mat dilateElement = getStructuringElement(MORPH_ELLIPSE, Size(10, 10));
 		dilate(filtered, filtered, dilateElement);
 
 		//fliping camera feeds
@@ -69,22 +57,108 @@ int main(int argc, char* argv[])
 		flip(HSV, HSV, 1);
 		flip(filtered, filtered, 1);
 
-		//vector for storing contours
-		vector< vector<Point> > contours;
+		//vectors
+		vector< vector<Point> > contours;								//vector for storing contours
+		vector < pair < double, vector < Point > > > sortedContours;	//vector for storing sorted contours
+
+
 
 		//finding contours
 		findContours(filtered, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
 
-		//drawing countours
 		for (unsigned i = 0; i < contours.size(); i++) {
-			drawContours(cameraFeed, contours, i, Scalar(200, 10, 30), 3);
+			drawContours(cameraFeed, contours, i, Scalar(200, 10, 30), 3); 				//drawing contours
+			sortedContours.push_back({ contourArea(contours[i], false),contours[i] });	//saving contours area and contours for sorting
 		}
 
+		if (sortedContours.size() > 0) {
+
+			Point2f pos;// circle center
+			float r;	// circle radius
+
+			//sorting conturs
+			sort(sortedContours.begin(), sortedContours.end(), [](auto a, auto b) {
+				return a.first > b.first;
+			});
+
+			minEnclosingCircle(sortedContours[0].second, pos, r); //searching for smallest possible circle to enclose our contured area
+			
+			if (r > 8) { //if radius of the circle is more than 8
+
+				if (path.size() < 70) {
+					path.push_back(pos);	//adding center point of the enclosing circle to back of our path list
+				}
+				else {
+					path.pop_front();		//adding center point of the enclosing circle to front of our path list
+					path.push_back(pos);	//adding center point of the enclosing circle to back of our path list
+				}
+
+				vector < Point > pathV;
+				vector < Point2f > approx;
+
+				approxPolyDP(vector<Point2f>(path.begin(), path.end()), approx, 60, false);
+
+				// Adding x,y parameters from circle center to path list
+				for (auto &p : approx) pathV.push_back({ (int)p.x,(int)p.y });
+
+				// Drawing line
+				polylines(cameraFeed, { pathV }, false, Scalar(232, 30, 0), 2);
+
+				if (pathV.size() >= 5) {
+					vector < Point > itr(pathV.end() - 5, pathV.end());
+					int conditions = 0;
+
+					//Drawing from the left side
+					if ((itr[0].x < itr[1].x) && (itr[0].y > itr[1].y)) {
+						conditions++;
+					
+						if ((itr[1].x < itr[2].x) && (itr[1].y < itr[2].y)) {
+							conditions++;
+						}
+						if ((itr[2].x < itr[3].x) && (itr[2].y > itr[3].y)) {
+							conditions++;
+						}
+						if ((itr[3].x < itr[4].x) && (itr[3].y < itr[4].y)) {
+							conditions++;
+						}
+					}
+
+					//Drawing from the right side
+					if ((itr[0].x > itr[1].x) && (itr[0].y > itr[1].y)) {
+						conditions++;
+
+						if ((itr[1].x > itr[2].x) && (itr[1].y < itr[2].y)) {
+							conditions++;
+						}
+						if ((itr[2].x > itr[3].x) && (itr[2].y > itr[3].y)) {
+							conditions++;
+						}
+						if ((itr[3].x > itr[4].x) && (itr[3].y < itr[4].y)) {
+							conditions++;
+						}
+					}
+
+					//Additional conditions to determin vertices are on similiar height. Point[0] is our mesure
+					if (  (itr[4].y > (itr[0].y * 0.8)) && (itr[4].y < (itr[0].y * 1.2))  ) {
+						conditions++;
+					}
+					
+
+					if (conditions == 5) {
+						cout << "Jest M!!" << endl;
+						imshow("Camera HSV", cameraFeed);
+
+						path.clear();
+					}
 
 
+				}
+
+				
+			}
+		}
 		//showing camera feeds
 		imshow("Camera", cameraFeed);
-		imshow("Camera HSV", HSV);
 		imshow("Filtered Camera", filtered);
 
 	
